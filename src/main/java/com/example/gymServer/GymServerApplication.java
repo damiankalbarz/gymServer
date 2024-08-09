d31a2650db15e004b0dbddb87488ad323942efc1
Damian <kalbarzd@gmail.com>
2024-08-09 14:29:28 +0200

    first commit

---
+++ src/main/java/com/example/gymServer/GymServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.gymServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gymServer/GymServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gymServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
