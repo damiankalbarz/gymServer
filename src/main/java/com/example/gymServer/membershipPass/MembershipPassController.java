a0952bb36b72763e07abd4bf11845081c9322f2d
Damian <kalbarzd@gmail.com>
2024-08-14 16:39:18 +0200

    membershipPass entity service and controller added

---
+++ src/main/java/com/example/gymServer/membershipPass/MembershipPassController.java
@@ -0,0 +1,29 @@
+package com.example.gymServer.membershipPass;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/membership-pass")
+public class MembershipPassController {
+
+    @Autowired
+    private MembershipPassService membershipPassService;
+
+    @GetMapping
+    public List<MembershipPass> getAllMembershipPasses() {
+        return membershipPassService.getAllMembershipPasses();
+    }
+
+    @PostMapping
+    public MembershipPass createMembershipPass(@RequestBody MembershipPass membershipPass) {
+        return membershipPassService.saveMembershipPass(membershipPass);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteMembershipPass(@PathVariable Long id) {
+        membershipPassService.deleteMembershipPass(id);
+    }
+}
